6e7553aa9f94d0d99c4c6a9660eafe3e3e9da453
Alan Harnum <aharnum@ocadu.ca>
2015-10-09 17:03:47 -0400

    Add a log flag, make a little better

--- dockpad.sh
+++ dockpad.sh
@@ -2,25 +2,33 @@
 
 # Project variables here
 
+DOCKPAD_IMAGE=aharnum/dockpad
 PROJECT_NAME=dockpad-test
-DOCKER_HUB_NAME=aharnum
+EXPORT_IMAGE_PREFACE=aharnum
 DOCS_TEMPLATE_REPO=https://github.com/jhung/docs-template
+
+# Terrible gigantic case statement as a control
 
 case "$1" in
     dev-start)
         echo "starting development container"
-        docker run --name $PROJECT_NAME -d -p 9778:9778 -v `pwd`/site-structure.json:/usr/src/app/site-structure.json -v `pwd`/src:/usr/src/app/src aharnum/dockpad
-        echo "Running at `docker-machine ip default`:9778"
+        docker run --name $PROJECT_NAME -d -p 9778:9778 -v `pwd`/site-structure.json:/usr/src/app/site-structure.json -v `pwd`/src:/usr/src/app/src $DOCKPAD_IMAGE
+        echo "Development site visible at `docker-machine ip default`:9778"
+        echo "Edit files in /src directory to customize site"
         ;;
     dev-restart)
         echo "restarting development container"
         docker restart $PROJECT_NAME
-        echo "Restarted, running at `docker-machine ip default`:9778"
+        echo "Restarted, visible at `docker-machine ip default`:9778"
         ;;
     dev-destroy)
         echo "destroying development container"
         docker stop $PROJECT_NAME && docker rm $PROJECT_NAME
         echo "development container destroyed"
+        ;;
+    dev-log)
+        echo "CTRL+C to stop following log"
+        docker logs -f $PROJECT_NAME
         ;;
     project-init)
         echo "performing initial project setup"
@@ -38,7 +46,7 @@ case "$1" in
     publish-package)
         docker exec -d $PROJECT_NAME docpad generate --env static
         docker cp $PROJECT_NAME:/usr/src/app/out out
-        docker build -t $DOCKER_HUB_NAME/$PROJECT_NAME .
+        docker build -t $EXPORT_IMAGE_PREFACE/$PROJECT_NAME .
         rm -r out
         ;;
     *)
